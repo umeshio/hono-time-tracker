a877058c3b642ec70ddf394d71bc695613b06609
mino <81564697+umeshio@users.noreply.github.com>
2026-06-17 19:27:59 +0900

    app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,18 @@
 import { useState } from 'react'
 import LoginPage from './pages/LoginPage'
+import DashboardPage from './pages/DashuboardPage'
 import './App.css'
-import { isLoggedIn as checkIsLoggedIn, removeToken } from './api/client'
+import { isLoggedIn as checkIsLoggedIn } from './api/client'
 
 function App() {
   // checkIsLoggedInはトークンの有無をbooleanで返す
   const [isLoggedIn, setIsLoggedIn] = useState(checkIsLoggedIn())
 
-  // ログアウト関数
-  function handleLogout() {
-    removeToken()
-    setIsLoggedIn(false)
-  }
-
   if(!isLoggedIn){
     return <LoginPage onLogin={() => setIsLoggedIn(true)} />
   }
 
-  return (
-    <div>
-      <h1>ダッシュボード</h1>
-      <button onClick={handleLogout}>ログアウト</button>
-    </div>
-  )
+  return <DashboardPage onLogout={() => setIsLoggedIn(false)} />
 }
 
 export default App
